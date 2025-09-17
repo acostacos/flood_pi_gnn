acf6e51dc029af77c43b34101963bf74b8d86227
acostacos <acostacarlom@gmail.com>
2025-09-17 19:23:18 +0800

    Modify experiment file to handle tests

--- run_experiment.sh
+++ run_experiment.sh
@@ -8,14 +8,14 @@
 
 . venv/bin/activate
 
-srun python train.py --config 'configs/autoregressive/2layer_config.yaml' --model 'NodeEdgeGNN'
-srun python train.py --config 'configs/autoregressive/4layer_config.yaml' --model 'NodeEdgeGNN'
-srun python train.py --config 'configs/autoregressive/6layer_config.yaml' --model 'NodeEdgeGNN'
-srun python train.py --config 'configs/autoregressive/8layer_config.yaml' --model 'NodeEdgeGNN'
-srun python train.py --config 'configs/autoregressive/10layer_config.yaml' --model 'NodeEdgeGNN'
+# srun python train.py --config 'configs/autoregressive/2layer_config.yaml' --model 'NodeEdgeGNN'
+# srun python train.py --config 'configs/autoregressive/4layer_config.yaml' --model 'NodeEdgeGNN'
+# srun python train.py --config 'configs/autoregressive/6layer_config.yaml' --model 'NodeEdgeGNN'
+# srun python train.py --config 'configs/autoregressive/8layer_config.yaml' --model 'NodeEdgeGNN'
+# srun python train.py --config 'configs/autoregressive/10layer_config.yaml' --model 'NodeEdgeGNN'
 
-# srun python test.py --config 'configs/autoregressive/2layer_config.yaml' --model 'NodeEdgeGNN' --model_path ''
-# srun python test.py --config 'configs/autoregressive/4layer_config.yaml' --model 'NodeEdgeGNN' --model_path ''
-# srun python test.py --config 'configs/autoregressive/6layer_config.yaml' --model 'NodeEdgeGNN' --model_path ''
-# srun python test.py --config 'configs/autoregressive/8layer_config.yaml' --model 'NodeEdgeGNN' --model_path ''
-# srun python test.py --config 'configs/autoregressive/10layer_config.yaml' --model 'NodeEdgeGNN' --model_path ''
+srun python test.py --config 'configs/autoregressive/2layer_config.yaml' --model 'NodeEdgeGNN' --model_path 'num_gnn_layers/model/NodeEdgeGNN_2025-09-17_17-21-54_2layer.pt'
+srun python test.py --config 'configs/autoregressive/4layer_config.yaml' --model 'NodeEdgeGNN' --model_path 'num_gnn_layers/model/NodeEdgeGNN_2025-09-17_17-40-39_4layer.pt'
+srun python test.py --config 'configs/autoregressive/6layer_config.yaml' --model 'NodeEdgeGNN' --model_path 'num_gnn_layers/model/NodeEdgeGNN_2025-09-17_18-03-32_6layer.pt'
+srun python test.py --config 'configs/autoregressive/8layer_config.yaml' --model 'NodeEdgeGNN' --model_path 'num_gnn_layers/model/NodeEdgeGNN_2025-09-17_18-30-49_8layer.pt'
+srun python test.py --config 'configs/autoregressive/10layer_config.yaml' --model 'NodeEdgeGNN' --model_path 'num_gnn_layers/model/NodeEdgeGNN_2025-09-17_19-02-44_10layer.pt'
