7e2acaeef29bb95e1ea98add86c170fae61522e0
acostacos <acostacarlom@gmail.com>
2025-10-03 23:32:11 +0800

    Commit changes

--- run_train.sh
+++ run_train.sh
@@ -4,9 +4,14 @@
 #SBATCH --partition=gpu-long
 #SBATCH --gpus=a100-40:1
 #SBATCH --mem-per-cpu=64000
-#SBATCH --time=720
+#SBATCH --time=2880
 
 . venv/bin/activate
 
-srun python train.py --config 'configs/config.yaml' --model 'GAT'
-srun python train.py --config 'configs/config.yaml' --model 'GCN'
+srun python train.py --config 'configs/global_local_weight_search/global_0.1_and_local_0.1_config.yaml' --model 'NodeEdgeGNN'
+srun python train.py --config 'configs/global_local_weight_search/global_0.1_and_local_0.01_config.yaml' --model 'NodeEdgeGNN'
+srun python train.py --config 'configs/global_local_weight_search/global_0.1_and_local_0.001_config.yaml' --model 'NodeEdgeGNN'
+srun python train.py --config 'configs/global_local_weight_search/global_0.1_and_local_0.0001_config.yaml' --model 'NodeEdgeGNN'
+
+# srun python train.py --config 'configs/config.yaml' --model 'GAT'
+# srun python train.py --config 'configs/config.yaml' --model 'GCN'
